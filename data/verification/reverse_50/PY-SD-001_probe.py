9398ffc231c8bf25837495010cf235514d1fbadc
RandomCatcobe <273058269+RandomCatcobe@users.noreply.github.com>
2026-05-21 22:32:06 +0800

    chore: snapshot full new state

---
+++ data/verification/reverse_50/PY-SD-001_probe.py
@@ -0,0 +1,9 @@
+import json, numpy as np
+a = np.float32(3) + 3.
+b = np.array([3], dtype=np.float32) + np.float64(3)
+print(json.dumps({
+    "scalar_dtype": str(np.asarray(a).dtype),
+    "scalar_value": repr(a),
+    "array_dtype": str(b.dtype),
+    "array_value": b.tolist(),
+}, sort_keys=True))
